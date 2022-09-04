cb52bf2ef57a7efda4e8ef5a65cfa1dee7d323c3
kaanoner3 <oner.kaan.k@gmail.com>
2022-09-04 13:19:05 +0300

    fix: App.tsx update

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { HomeScreen } from '@skoove/home.content'
-import { MusicContentScreen } from '@skoove/music-player.content'
-import { ScreenHOC } from '@skoove/platform.screen'
+import { MusicPlayerScreen } from '@skoove/music-player.content'
+import { store } from '@skoove/platform.redux'
+import { ScreenHOC, PlayerSetupHOC } from '@skoove/platform.screen'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
-import { store } from '@skoove/platform.redux'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,17 +18,19 @@ const queryClient = new QueryClient({
 const Stack = createNativeStackNavigator()
 
 const Home = ScreenHOC(HomeScreen)
-const MusicContent = ScreenHOC(MusicContentScreen)
+const MusicContent = ScreenHOC(MusicPlayerScreen)
 
 export default function App() {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Songs" component={Home} />
-            <Stack.Screen name="MusicContent" component={MusicContent} />
-          </Stack.Navigator>
+          <PlayerSetupHOC>
+            <Stack.Navigator>
+              <Stack.Screen name="Songs" component={Home} />
+              <Stack.Screen name="MusicContent" component={MusicContent} />
+            </Stack.Navigator>
+          </PlayerSetupHOC>
         </NavigationContainer>
       </QueryClientProvider>
     </Provider>
